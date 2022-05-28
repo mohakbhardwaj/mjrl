83dc502b791cdc982a8976c9f7aef889f3590220
chinganc <chinganc0@gmail.com>
2022-05-28 12:34:35 -0700

    Add requirements.

--- install_mujoco.sh
+++ install_mujoco.sh
@@ -1,4 +1,4 @@
-sudo apt-get install libglew-dev patchelf
+sudo apt-get install libglew-dev patchelf -y
 wget https://github.com/deepmind/mujoco/releases/download/2.1.1/mujoco-2.1.1-linux-x86_64.tar.gz
 wget https://github.com/deepmind/mujoco/releases/download/2.1.0/mujoco210-linux-x86_64.tar.gz
 tar -xf mujoco-2.1.1-linux-x86_64.tar.gz
