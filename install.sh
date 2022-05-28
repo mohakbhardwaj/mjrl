a6859875308d5e2055e8758ce1fd671b48676887
chinganc <chinganc0@gmail.com>
2022-05-28 13:02:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 
+# Clone the repo
+# git clone -b v2 https://github.com/mohakbhardwaj/mjrl.git
+# cd mjrl
+# . install.sh
 
+conda create -n pmpc python=3.7 -y
+conda activate pmpc
+# . install_mujoco.sh  # install mujoco, if missing
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mujoco210/bin:/usr/lib/nvidia
+pip install -r requirements.txt
+pip install -e  .
+
+
+
+# old one
 # git clone https://github.com/mohakbhardwaj/mjrl.git
 # cd mjrl
 # git checkout v2
@@ -11,15 +25,3 @@
 # pip install git+https://github.com/rail-berkeley/d4rl@master#egg=d4rl
 # pip install -e .
 # python prep_d4rl_dataset.py  --env_name hopper-medium-v2
-
-
-
-conda create -n pmpc python=3.7
-conda activate pmpc
-git clone https://github.com/mohakbhardwaj/mjrl.git
-cd mjrl
-git checkout v2
-# . install_mujoco.sh  # install mujoco, if missing
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mujoco210/bin:/usr/lib/nvidia
-pip install -r requirements.txt
-pip insatll -e  .
